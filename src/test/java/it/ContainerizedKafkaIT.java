1145d7141dd4c17e17c9098c7af403b4dc9c9ce3
Micha Kops <micha.kops@gmail.com>
2019-01-27 20:27:17 +0100

    Kafka client lib added

--- src/test/java/it/ContainerizedKafkaIT.java
+++ src/test/java/it/ContainerizedKafkaIT.java
@@ -3,6 +3,11 @@ package it;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Properties;
+import java.util.stream.IntStream;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.KafkaContainer;
@@ -17,7 +22,27 @@ public class ContainerizedKafkaIT {
 
   @Test
   @DisplayName("kafka it should be running")
-  void kafkaShouldBeRunning() throws Exception {
+  void shouldBeRunningKafka() throws Exception {
     assertTrue(kafkaContainer.isRunning());
+  }
+
+  @Test
+  @DisplayName("should send records over kafka container")
+  void shouldSendAndReceiveMessages() throws Exception {
+    String servers = kafkaContainer.getBootstrapServers();
+    System.out.printf("servers: %s%n", servers);
+
+    Properties props = new Properties();
+    props.put("bootstrap.servers", servers);
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    try (
+        Producer<String, String> producer = new KafkaProducer<>(props)) {
+      IntStream.range(0,100).forEach(i -> {
+        final String msg = String.format("my-message-%d", i);
+        producer.send(new ProducerRecord<>("my-topic", msg));
+        System.out.println("Sent:" + msg);
+      });
+    }
   }
 }
