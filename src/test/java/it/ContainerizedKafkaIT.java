5659efbad1e1367990a14a734e6bf24ac95b9da8
Micha Kops <micha.kops@gmail.com>
2019-01-27 20:50:03 +0100

    Producer/consumer example added

--- src/test/java/it/ContainerizedKafkaIT.java
+++ src/test/java/it/ContainerizedKafkaIT.java
@@ -1,10 +1,17 @@
 package it;
 
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -36,6 +43,24 @@ public class ContainerizedKafkaIT {
     props.put("bootstrap.servers", servers);
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    props.put("group.id", "group-1");
+
+    final AtomicInteger counter = new AtomicInteger(0);
+
+    new Thread(() -> {
+      KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
+      kafkaConsumer.subscribe(Arrays.asList("my-topic"));
+      while (true) {
+        ConsumerRecords<String, String> records = kafkaConsumer.poll(20);
+        records.forEach(record -> {
+          System.out.printf("%d # offset: %d, value = %s%n", counter.incrementAndGet(), record.offset(), record.value());
+        });
+      }
+
+    }).start();
+
     try (
         Producer<String, String> producer = new KafkaProducer<>(props)) {
       IntStream.range(0,100).forEach(i -> {
